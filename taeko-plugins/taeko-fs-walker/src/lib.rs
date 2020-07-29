ca3bde2bce8b5e33b24c82e72dbf0cc06a1ad731
Dustin Knopoff <dustinknopoff@gmail.com>
2020-07-29 13:36:05 -0400

    feat(taeko-fs-walker): a wrapper around WalkDir for use in taeko

---
+++ taeko-plugins/taeko-fs-walker/src/lib.rs
@@ -0,0 +1,46 @@
+#![warn(rust_2018_idioms)]
+#![warn(clippy::all)]
+//!
+use std::sync::Arc;
+use walkdir::WalkDir;
+use std::path::Path;
+
+#[derive(Default)]
+pub struct FSWalker;
+
+type WalkContents = (String,Arc<String>);
+
+impl FSWalker {
+    pub fn walk<P>(&mut self, file_types: &[&str], root: P) -> Vec<WalkContents> 
+        where P: AsRef<Path> {
+        WalkDir::new(root.as_ref().canonicalize().unwrap())
+            .into_iter()
+            .filter_map(|e| e.ok())
+            .filter(|e|{
+                if e.path().is_file() {
+                    let extension = e.path().extension().unwrap().to_str().unwrap();
+                    file_types.iter().any(|w| w == &extension)
+                } else {
+                    false
+                }
+        })
+        .map(|entry| (
+            entry.path().to_str().unwrap().to_string(), 
+            Arc::new(std::fs::read_to_string(entry.path().to_str().unwrap().to_string()).unwrap()))
+        )
+        .collect()
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let mut walker = FSWalker::default();
+        let contents = walker.walk(&["md"], "./test_content/");
+        assert_eq!(12, contents.len());
+    }
+}
