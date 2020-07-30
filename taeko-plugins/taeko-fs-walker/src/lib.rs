a378135c8eb64cfedf82d50f4877bb60fcd1e568
Dustin Knopoff <dustinknopoff@gmail.com>
2020-07-30 11:19:40 -0400

    fix(walker): export type for walking output
    - Formatting

--- taeko-plugins/taeko-fs-walker/src/lib.rs
+++ taeko-plugins/taeko-fs-walker/src/lib.rs
@@ -1,37 +1,43 @@
 #![warn(rust_2018_idioms)]
 #![warn(clippy::all)]
 //!
+use std::path::Path;
 use std::sync::Arc;
 use walkdir::WalkDir;
-use std::path::Path;
 
 #[derive(Default)]
 pub struct FSWalker;
 
-type WalkContents = (String,Arc<String>);
+pub type WalkContents = (String, Arc<String>);
 
 impl FSWalker {
-    pub fn walk<P>(&mut self, file_types: &[&str], root: P) -> Vec<WalkContents> 
-        where P: AsRef<Path> {
+    pub fn walk<P>(&mut self, file_types: &[&str], root: P) -> Vec<WalkContents>
+    where
+        P: AsRef<Path>,
+    {
         WalkDir::new(root.as_ref().canonicalize().unwrap())
             .into_iter()
             .filter_map(|e| e.ok())
-            .filter(|e|{
+            .filter(|e| {
                 if e.path().is_file() {
                     let extension = e.path().extension().unwrap().to_str().unwrap();
                     file_types.iter().any(|w| w == &extension)
                 } else {
                     false
                 }
-        })
-        .map(|entry| (
-            entry.path().to_str().unwrap().to_string(), 
-            Arc::new(std::fs::read_to_string(entry.path().to_str().unwrap().to_string()).unwrap()))
-        )
-        .collect()
+            })
+            .map(|entry| {
+                (
+                    entry.path().to_str().unwrap().to_string(),
+                    Arc::new(
+                        std::fs::read_to_string(entry.path().to_str().unwrap().to_string())
+                            .unwrap(),
+                    ),
+                )
+            })
+            .collect()
     }
 }
-
 
 #[cfg(test)]
 mod tests {
